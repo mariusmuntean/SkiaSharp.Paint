20a200c2120c7bac82e4a01605e1fb4f5a2d2d23
Marius Muntean <marius.muntean@live.com>
2020-06-08 17:21:55 +0200

    Added an important hint to the documentation

--- src/SkiaSharp.Paint/IPaintChannelWriter.cs
+++ src/SkiaSharp.Paint/IPaintChannelWriter.cs
@@ -6,12 +6,14 @@ namespace SkiaSharp.Paint
     {
         /// <summary>
         /// Sends the given <see cref="PaintMessage"/> to the <see cref="IPaintChannel"/> that created this <see cref="IPaintChannelWriter"/>
+        /// <para>Sending a new <see cref="PaintMessage"/> with an existing <see cref="PaintMessage.ActionId"/> will replace the previous <see cref="PaintMessage"/> with the same <see cref="PaintMessage.ActionId"/>. This allow refreshing some content on the canvas. </para>
         /// </summary>
         /// <param name="paintMessage"></param>
         void Dispatch(PaintMessage paintMessage);
 
         /// <summary>
         /// Asynchronously sends the given <see cref="PaintMessage"/> to the <see cref="IPaintChannel"/> that created this <see cref="IPaintChannelWriter"/>
+        /// <para>Sending a new <see cref="PaintMessage"/> with an existing <see cref="PaintMessage.ActionId"/> will replace the previous <see cref="PaintMessage"/> with the same <see cref="PaintMessage.ActionId"/>. This allow refreshing some content on the canvas. </para>
         /// </summary>
         /// <param name="paintMessage"></param>
         Task DispatchAsync(PaintMessage paintMessage);
